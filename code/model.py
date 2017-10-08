0372251d060dda72e89d52c47680534de46d209e
Alexander Bondarchuk <al.v.bondarchuk@gmail.com>
2017-10-08 23:33:19 +0200

    Different model size

--- code/model.py
+++ code/model.py
@@ -7,6 +7,7 @@ import time
 
 
 BATCH_SIZE = 20
+EPOCHS = 5
 
 model = create_model()
 model.compile("adam", "mse")
@@ -22,11 +23,12 @@ v_seq = DrivingDataSequence(data_container.validation_data, BATCH_SIZE)
 
 print("Number of training examples: ", t_seq.steps_per_epoch*BATCH_SIZE)
 print("Number of validation examples: ", v_seq.steps_per_epoch*BATCH_SIZE)
+print("{} epochs with {} batch size".format(EPOCHS, BATCH_SIZE))
 
 start_time = time.time()
 
 model.fit_generator(t_seq, t_seq.steps_per_epoch,
-                    epochs=5, callbacks=[LambdaCallback(on_epoch_end=data_container.training_data.shuffle)],
+                    epochs=EPOCHS, callbacks=[LambdaCallback(on_epoch_end=data_container.training_data.shuffle)],
                     validation_data=v_seq, validation_steps=v_seq.steps_per_epoch)
 
 model.save("../model.h5")
